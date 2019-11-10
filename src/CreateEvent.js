986b473cd91c88bee203c673b2c4aeef852f479e
Michael Pontus <m.pontus@gmail.com>
2019-11-10 09:27:45 +0300

    Update CreateEvent backgronud

--- src/CreateEvent.js
+++ src/CreateEvent.js
@@ -6,9 +6,8 @@ import { Button } from "./components/Button/Button";
 import { Input } from "./components/Input/Input";
 import { useInteractor } from "./hooks/useInteractor";
 import { FullPageTransition } from "./components/FullPageTransition/FullPageTransition";
-import Dropzone from "react-dropzone";
 import QRCode from "qrcode-react";
-import { ImageUpload } from "./components/ImageUpload/ImageUpload";
+import backgroundSrc from "./polygon-background.jpg";
 
 export const CreateEvent = ({ createEvent }) => {
   const [isSubmitting, handleSubmit] = useInteractor(createEvent);
@@ -31,41 +30,43 @@ export const CreateEvent = ({ createEvent }) => {
   );
 
   return (
-    <Layout style={{ textAlign: "center" }}>
-      <FullPageTransition
-        open={result !== undefined}
-        to={
-          <Layout>
-            <QRCode value={checkoutUrl} size={420} />
-          </Layout>
-        }
-      >
-        <form onSubmit={formik.handleSubmit}>
-          <Input
-            autoFocus
-            name="name"
-            label={<Trans>Event Name</Trans>}
-            required
-            onChange={formik.handleChange}
-            value={formik.values.name}
-          />
-          <Input
-            autoFocus
-            name="redirectUrl"
-            label={<Trans>Redirect URL</Trans>}
-            required
-            onChange={formik.handleChange}
-            value={formik.values.redirectUrl}
-          />
-          <Button
-            type="submit"
-            isSubmitting={isSubmitting}
-            style={{ marginTop: "15px" }}
-          >
-            <Trans>Create Event</Trans>
-          </Button>
-        </form>
-      </FullPageTransition>
-    </Layout>
+    <div style={{ backgroundImage: `url("${backgroundSrc}")` }}>
+      <Layout style={{ textAlign: "center" }}>
+        <FullPageTransition
+          open={result !== undefined}
+          to={
+            <Layout>
+              <QRCode value={checkoutUrl} size={420} />
+            </Layout>
+          }
+        >
+          <form onSubmit={formik.handleSubmit}>
+            <Input
+              autoFocus
+              name="name"
+              label={<Trans>Event Name</Trans>}
+              required
+              onChange={formik.handleChange}
+              value={formik.values.name}
+            />
+            <Input
+              autoFocus
+              name="redirectUrl"
+              label={<Trans>Redirect URL</Trans>}
+              required
+              onChange={formik.handleChange}
+              value={formik.values.redirectUrl}
+            />
+            <Button
+              type="submit"
+              isSubmitting={isSubmitting}
+              style={{ marginTop: "15px" }}
+            >
+              <Trans>Create Event</Trans>
+            </Button>
+          </form>
+        </FullPageTransition>
+      </Layout>
+    </div>
   );
 };
